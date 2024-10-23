f5720fabc6f6c4767bd90c8a37a2807876d9910a
Aoi Emerauda <aoi@emerauda.com>
2024-10-23 18:55:06 +0900

    Update default genesis block configuration

--- core/genesis.go
+++ core/genesis.go
@@ -332,24 +332,15 @@ func GenesisBlockForTesting(db ethdb.Database, addr common.Address, balance *big
 	return g.MustCommit(db)
 }
 
+// DefaultGenesisBlock returns the Ethereum main net genesis block.
 func DefaultGenesisBlock() *Genesis {
 	return &Genesis{
-		Config: &params.ChainConfig{
-			ChainID: big.NewInt(329),
-			HomesteadBlock: big.NewInt(0),
-			EIP150Block: big.NewInt(0),
-			EIP155Block: big.NewInt(0),
-			EIP158Block: big.NewInt(0),
-			ByzantiumBlock: big.NewInt(0),
-			ConstantinopleBlock: big.NewInt(0),
-		},
+		Config:     params.MainnetChainConfig,
 		Nonce:      0x000000000133c888,
 		ExtraData:  hexutil.MustDecode("0x"),
 		GasLimit:   134217728,
 		Difficulty: big.NewInt(1024),
-		Alloc: map[common.Address]GenesisAccount{
-			common.HexToAddress("0x950302976387b43E042aeA242AE8DAB8e5C204D1"): {Balance: new(big.Int).SetString("10000000000000000000000000", 10)},
-		},
+		Alloc:      decodePrealloc(mainnetAllocData),
 	}
 }
 
