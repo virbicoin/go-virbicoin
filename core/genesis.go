e2febcb2a3ef78f05ed673b5373c99503e3ef8f3
Aoi Emerauda <aoi@emerauda.com>
2024-10-26 07:23:19 +0900

    Adjust balance of a specific account in default genesis block configuration

--- core/genesis.go
+++ core/genesis.go
@@ -342,7 +342,7 @@ func DefaultGenesisBlock() *Genesis {
 		Difficulty: big.NewInt(1024),
         Alloc: map[common.Address]GenesisAccount{
             common.HexToAddress("0x950302976387b43E042aeA242AE8DAB8e5C204D1"): {
-                Balance: big.NewInt(5000000000000000000),
+				Balance: big.NewInt(8888888888888888888),
             },
         },
 	}
