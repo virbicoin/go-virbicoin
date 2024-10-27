d703a5b3b6f61a5335220a1f081a0fd8c0ad4169
Aoi Emerauda <aoi@emerauda.com>
2024-10-27 19:23:54 +0900

    Update default genesis block configuration: Add new bootnode in AWS ap-northeast-3a

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -22,8 +22,9 @@ import "github.com/ethereum/go-ethereum/common"
 // the main Ethereum network.
 var MainnetBootnodes = []string{
 	// VirBiCoin Go Bootnodes
-	"enode://a96b1e0d1e93987a81a08dcef602890d25e449ba2823b03bfb521cf4e72020e6af86ae87c6aae89b62d57a7ec380bced83108fb8810205db508bddf32f98f6e7@35.72.202.199:28329", // bootNode-AWS-ap-northeast-1c-001
+	"enode://a96b1e0d1e93987a81a08dcef602890d25e449ba2823b03bfb521cf4e72020e6af86ae87c6aae89b62d57a7ec380bced83108fb8810205db508bddf32f98f6e7@35.72.202.199:28329", // bootNode-AWS-ap-northeast-1a-001
 	"enode://156c3707f0a68d1a38e398b32ba11529201d37be49f5990ca3294cadac2870f6c08b3db604dbdf306f93d1128589631356095c59121acab37e8be7c56ba75931@52.69.140.99:28329", // bootNode-AWS-ap-northeast-1d-001
+	"enode://afb0a6b6a0a9b9881a384bea5c98f1997d0a0db7ae4ea89434e9b87ff65eb5e3f9a096d9ab757718f2915441f6d6ba8ab0ca9a6c452e1d7be9804bbcdf8ba662@13.208.46.232:28329", // bootNode-AWS-ap-northeast-3a-001
 }
 
 // RopstenBootnodes are the enode URLs of the P2P bootstrap nodes running on the
