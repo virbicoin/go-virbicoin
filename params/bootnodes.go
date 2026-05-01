8b1be3f74d39cd2fe7409acd0b3453f890406963
Aoi Emerauda <aoi@emerauda.com>
2026-05-02 01:13:34 +0900

    v1.9.37 リリース: VersionMetaをstableに変更

--- params/bootnodes.go
+++ params/bootnodes.go
@@ -22,7 +22,7 @@ import "github.com/virbicoin/go-virbicoin/common"
 // the main Ethereum network.
 var MainnetBootnodes = []string{
 
-    // VirBiCoin Official Go Bootnodes
+	// VirBiCoin Official Go Bootnodes
 	"enode://e8853097e51e155d6bac72b9ebff92ccdf5aa8ee9a845b92f6af969120b9ed017d6b79f231cf05f68e68237a824946f502b84914e0737e81c7a252a0174764f8@140.238.40.136:28329", // bootnode-oracle-ap-tokyo-1-01
 	"enode://141e23b0dc593a3e3474485d470df0057f2d710cd2d5dec733a7f2f8e6df81b034c4f5ff65e192af604c0d0f8bf2c53d2671190e3c9dcf78ada91e8810a43e54@158.101.85.149:28329", // bootnode-oracle-ap-tokyo-1-02
 }
